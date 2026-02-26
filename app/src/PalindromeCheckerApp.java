ac298c5cca455a9b63fd71210f5d112d18cb656c
hrijit123 <hrijit.b@gmail.com>
2026-02-26 16:08:31 +0530

    UC6: Queue + Stack Based Palindrome Check

--- app/src/PalindromeCheckerApp.java
+++ app/src/PalindromeCheckerApp.java
@@ -1,51 +1,46 @@
+import java.util.Stack;
 /**
- * =============================================================
- * MAIN CLASS - UseCase4PalindromeCheckerApp
- * =============================================================
- *
- * Use Case 4: Character Array Based Validation
- *
- * Description:
- * This class validates a palindrome by converting
- * the string into a character array and comparing
- * characters using the two-pointer technique.
- *
- * At this stage, the application:
- * - Converts string to char array
- * - Uses start and end pointers
- * - Compares characters efficiently
+ * ======================================================================
+ * MAIN CLASS - UseCase5PalindromeCheckerApp
+ * ======================================================================
+ * * Use Case 5: Stack Based Palindrome Checker
+ * * Description:
+ * This class validates a palindrome using a Stack
+ * data structure which follows the LIFO principle.
+ * * At this stage, the application:
+ * - Pushes characters into a stack
+ * - Pops them in reverse order
+ * - Compares with original sequence
  * - Displays the result
- *
- * This reduces extra memory usage.
- *
- * @author Developer
- * @version 4.0
+ * * This maps stack behavior to reversal logic.
+ * * @author Developer
+ * @version 5.0
  */
 
 public class PalindromeCheckerApp {
 
     /**
-     * Application entry point for UC4.
-     *
-     * @param args Command-line arguments
+     * Application entry point for UC5.
+     * * @param args Command-line arguments
      */
-    public static boolean isPalindrome(String str) {
-        char[] arr = str.toCharArray();
-        int left = 0;
-        int right = arr.length - 1;
 
-        while (left < right) {
-            if (arr[left] != arr[right]) {
+    public static boolean isPalindrome(String input) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : input.toCharArray()) {
+            stack.push(ch);
+        }
+
+        for (char ch : input.toCharArray()) {
+            if (ch != stack.pop()) {
                 return false;
             }
-            left++;
-            right--;
         }
         return true;
     }
 
     public static void main(String[] args) {
-        String input = "madam";
-        System.out.println(isPalindrome(input));
+        String test = "level";
+        System.out.println(isPalindrome(test));
     }
 }
